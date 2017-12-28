803ae35425bc2a6651dc73ffb99651e25220650f
Gabriel Afflitto <gafflitto92@gmail.com>
2017-12-27 17:37:02 -0700

    written all tests for user stories

--- spec/features/user_visits_movie_index_spec.rb
+++ spec/features/user_visits_movie_index_spec.rb
@@ -5,16 +5,19 @@ describe "user can visit movie index" do
     @movie = Movie.create!(title: "Kill Bill", description: "Badass revenge flick")
     @movie2 = Movie.create!(title: "Reservoir Dogs", description: "Heist gone wrong")
     @movie3 = Movie.create!(title: "Pulp Fiction", description: "Crime stories")
-    @movie4 = Movie.create!(title: "Django: Unchained", description: "Freed slave revenge")
     @user = User.create!(name: "Gabe")
+    @user2 = User.create!(name: "Bill")
+    @user3 = User.create!(name: "Jill")
     @review = @user.reviews.create!(rating: 1, movie: @movie)
     @review2 = @user2.reviews.create!(rating: 1, movie: @movie)
     @review3 = @user3.reviews.create!(rating: 1, movie: @movie)
-    @user2 = User.create!(name: "Bill")
     @review4 = @user.reviews.create!(rating: 0, movie: @movie2)
     @review5 = @user2.reviews.create!(rating: 1, movie: @movie2)
-    @review6 = @user3.reviews.create!(rating: 0, movie: @movie2)
-    @user3 = User.create!(name: "Jill")
+    @review6 = @user3.reviews.create!(rating: 1, movie: @movie2)
+    @review7 = @user.reviews.create!(rating: 0, movie: @movie3)
+    @review8 = @user2.reviews.create!(rating: 0, movie: @movie3)
+    @review9 = @user3.reviews.create!(rating: 0, movie: @movie3)
+
     visit movies_path
   end
 
@@ -27,6 +30,16 @@ describe "user can visit movie index" do
   it "should show the most favorited movie in a header" do
     within ".favorite-movie" do
       expect(page).to have_content("The most favorited movie is Kill Bill")
+      expect(page).to have_content("Badass revenge flick")
+      expect(page).to have_content("Favorited by 3 users")
+    end
+  end
+
+  it "should show the least favorited movie in a header" do
+    within ".least-favorite" do
+      expect(page).to have_content("The least favorited movie is Pulp Fiction")
+      expect(page).to have_content("Crime stories")
+      expect(page).to have_content("Favorited by 3 users")
     end
   end
 end
